e012db4790266fce852eea1256b6330e0edcf34e
LuisC <72262647+LuisC-web@users.noreply.github.com>
2025-07-19 11:16:53 -0500

    Añadiendo vista de Productos

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,17 @@
+import ProductTable from "@/src/components/products/ProductsTable";
+import Heading from "@/src/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+import React from "react";
+const getProducts = async () => await prisma.product.findMany();
+
+async function OrderProducts() {
+  const products = await getProducts();
+  return (
+    <>
+      <Heading>Administrar ordenes</Heading>
+      <ProductTable products={products}></ProductTable>
+    </>
+  );
+}
+
+export default OrderProducts;
