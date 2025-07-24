d3246655cb641c5718b70a19cd741db7ec0941cd
LuisC <72262647+LuisC-web@users.noreply.github.com>
2025-07-24 06:05:28 -0500

    añadiendo paginacion

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,16 +1,37 @@
+import ProductsPagination from "@/src/components/products/ProductsPagination";
 import ProductTable from "@/src/components/products/ProductsTable";
 import Heading from "@/src/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
-const getProducts = async () =>
-  await prisma.product.findMany({ include: { category: true } });
+const PAGE_SIZE = 10;
+const getProducts = async (page: number, pageSize: number = 10) =>
+  await prisma.product.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+    include: { category: true },
+  });
+  const prdouctCount = async () =>
+    await prisma.product.count()
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
-async function OrderProducts() {
-  const products = await getProducts();
+async function OrderProducts({
+  searchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) {
+  const searchPage = await searchParams;
+  const page = +searchPage.page || 1;
+  if(page<0) redirect("/admin/products")
+  const [products, totalProducts] = await Promise.all([getProducts(page,PAGE_SIZE), prdouctCount()])
+  const totalPages = Math.ceil(totalProducts / PAGE_SIZE)
+  if (page > totalPages) {
+    redirect("/admin/products")
+  }
   return (
     <>
       <Heading>Administrar ordenes</Heading>
       <ProductTable products={products}></ProductTable>
+      <ProductsPagination page={page} totalPages={totalPages}/> 
     </>
   );
 }
---
+++ src/components/products/ProductsPagination.tsx
@@ -0,0 +1,21 @@
+import {  ArrowLeft, ArrowRight } from "lucide-react";
+import Link from "next/link"
+type ProductsPaginationProps = {
+  page: number;
+  totalPages: number;
+}
+function ProductsPagination({ page, totalPages }: ProductsPaginationProps) {
+  const pages=Array.from({length:totalPages},(_,i)=>i+1
+  )
+  return (
+    <nav className="flex justify-center py-10 gap-1 items-center">
+      <Link href={`/admin/products?page=${page>1?page-1:1}`} className="cursor-pointer"><ArrowLeft></ArrowLeft></Link>
+      {pages.map(pageArray =>
+        <Link href={`/admin/products?page=${pageArray}`} className={`cursor-pointer  p-2  transition-colors rounded ${pageArray===page?"bg-gray-300 font-black":"bg-white hover:bg-gray-300"}`}>{ pageArray}</Link>
+      )}
+      <Link href={`/admin/products?page=${page<totalPages?page+1:totalPages}`} className="cursor-pointer"><ArrowRight></ArrowRight></Link>
+    </nav>
+  )
+}
+
+export default ProductsPagination
